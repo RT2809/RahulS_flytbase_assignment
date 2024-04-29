e383ed0d3f34af7e142e3f07b08b978bc972f774
RT2809 <168326433+RT2809@users.noreply.github.com>
2024-04-29 08:59:39 +0530

    Create triangle_trajectory.py

---
+++ triangle_trajectory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import time
+from flyt_python import api
+
+drone = api.navigation(timeout=120000)  # instance of flyt droneigation cla$
+
+time.sleep(3)
+
+print 'taking off till 10m'
+drone.take_off(10.0)
+
+print ' move in a triangular trajectory of side length 10m at a height of 10m'
+
+print ' A to B of triangle ABC'
+# gives us the desired side length of 10m.
+drone.position_set(0, 0, 10, relative=True)
+
+print ' B to C of triangle ABC'
+drone.position_set(5, 8.66, 0, relative=True)
+
+print '  C to A of triangle ABC'
+drone.position_set(-5, 8.66, 0, relative=True)
+
+print ' Landing'
+drone.land(async=False)
+
+# shutdown the instance
+drone.disconnect()
